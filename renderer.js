990e7b1783320e801f50bbf55938ad92bee02abd
Eric Baker <elb5465@gmail.com>
2020-08-07 18:33:46 -0400

    Update renderer.js
    got some of the information displayed  
    in tabs now for the electron app, and it also reads the
     file that is selected using node.js, displaying original log in last tab

--- renderer.js
+++ renderer.js
@@ -6,8 +6,10 @@ var fs = require('fs');
 const { promisify } = require('util');
 const sleep = promisify(setTimeout);
 const { exec } = require("child_process");
-
-
+var fs = require('fs');
+var execute = function(command, callback){
+  exec(command, {maxBuffer: 1024 * 1000}, function(error, stdout, stderr){ callback(error, stdout); });
+};
 var file = "";
 
 
@@ -27,38 +29,128 @@ function selectFile(){
 
 
 
+
+//--------------- CODE TO INCLUDE JSON IN OUTPUT ---------------------
+function createJSON(){
+  var checkbox = document.getElementById("checkbox");
+  var json_div = document.getElementById("json_div");
+  var border = document.getElementById("border");
+  json = checkbox.checked;
+
+  // console.log(pubkey_div.style['display']);
+
+  if (checkbox.checked){
+    json_div.style['display'] = 'block';
+    // border.style['display'] = 'block';
+  }
+  else{
+    document.getElementById("json_results").value = "";
+    json_div.style['display'] = 'none';
+    border.style['display'] = 'none';
+  }
+}
+//-------------------------------------------------------------------------
+
+
+
+
+
 //---------------- CODE TO INITIATE SCRIPT RUNNING ON BUTTON CLICK ----------------
 function call_PyScript(){  
+    var res = document.getElementById('results_contents').value;
+    log = document.getElementById("filePath").value;
+    start_collecting_summary = false;
+    start_collecting_json = false;
+    
 
-    var res = document.getElementById('results');
-    file = document.getElementById("filePath").value;
-
-    console.log(`${file}`);
-
-    if (file == "undefined"){
+    if (log == "undefined"){
       file = "";
     }
 
-    //TK - Make code in py script to take argument of a particular file path, 
-    //     and have it default to doing all the files in the current directory if none are given
-    // exec("python M0_log_parser.py" + file, (error, stdout, stderr) => {
-    exec("python log_parser.py " + file, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+    if (document.getElementById("checkbox").checked){
+      log += " json";
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-  // }
 
+    execute("python parsing_folder/log_parser.py " + log, (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          document.getElementById('results_contents').value = `error: ${error.message}`;
+          // if (error.message.contains())
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          document.getElementById('results_contents').value = `stderr: ${stderr}`;
+          return;
+      }
 
-    // TK - Do work here to call python script
+      list_of_lines = stdout.split("\n");
+
+      for (i=0; i < list_of_lines.length; i++){
+        line = list_of_lines[i].trim();
+
+        if (line == "LOG RESULTS SUMMARY:"){
+          start_collecting_summary = true;
+        }
+        console.log(`line: ${line}`);
+        // document.getElementById('summary_contents').innerHTML += list_of_lines[i] + "<br>";
+
+        
+      }
+
+      document.getElementById('results_contents').value = `stdout: ${stdout}`;
+      document.getElementById('results_contents').innerHTML = `stdout: ${stdout}`;
+
+    });
+
 }
 //-------------------------------------------------------------------------
+
+
+document.addEventListener('click', ({ target }) => {
+  if (!target.closest('.tablinks')) {
+    console.log('click outside')
+    // reset the tabs when clicking away
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+  }
+})
+
+
+
+//---------------- FUNCTIONALITY OF TABS CHANGING --------------
+function openTab(evt, tab) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  if (tab=="test_log"){
+    if (file!=""){
+        fs.readFile(file[0], 'utf8', function(err, data) {
+            if (err) throw err;
+            console.log(data);
+            document.getElementById("log_contents").innerHTML = data;
+        });                    
+    }
+  }
+}
+//-------------------------------------------------------------------------
+
 
 
 
@@ -69,10 +161,13 @@ function clearSelections(all=0){
 
   file = "";
   document.getElementById("filePath").value = "";
+  document.getElementById("checkbox").checked = false;
+
 
   //only reset the responses when specified
   if (all==0){
-    document.getElementById("results").value = "";
+    document.getElementById("results_contents").value = "";
+    document.getElementById("log_contents").innerHTML = "Select a file above to view its contents here";
   }
 } 
 //-------------------------------------------------------------------------
