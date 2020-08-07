278ee7aa3929b911fe4d69a85ec532c73418e506
Eric Baker <elb5465@gmail.com>
2020-08-07 18:34:22 -0400

    Update main.js
    got some of the information displayed  
    in tabs now for the electron app, and it also reads the
     file that is selected using node.js, displaying origin
    al log in last tab

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ let mainWindow
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 615,
-        height: 800,
+        width: 800,
+        height: 900,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname, 'preload.js')
